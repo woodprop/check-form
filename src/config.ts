f5bd9565f7f027b13476179a328e78348d993edc
Konstantin <woodprop@yandex.ru>
2025-03-23 23:09:59 +0300

    feat: add constants for test and deploy Urls in config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
+// @ts-ignore
+const testUrl = 'https://script.google.com/macros/s/AKfycbxVWlAlL95CIkBJrqcjrzmFeke7FBav_0RH7UN_3t0bWa5Z0LRYOnngxNbHI9jKR_INfw/exec';
+// @ts-ignore
+const deployUrl = 'https://script.google.com/a/macros/silavetra.team/s/AKfycbz90EYv3iWvXyaK9woU3nsBoRjDYfvmtzWF1vpRX6CnanQzp6aU_LLAvHagbqeKwQ/exec';
+
 export const config = {
-    url: 'https://script.google.com/macros/s/AKfycbxVWlAlL95CIkBJrqcjrzmFeke7FBav_0RH7UN_3t0bWa5Z0LRYOnngxNbHI9jKR_INfw/exec',
+    url: testUrl,
+    // url: deployUrl,
     sheets: {
         tech: 'Техно',
         marketing: 'Маркетинг',
